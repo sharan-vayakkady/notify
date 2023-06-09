c1ed0627adbcab965bbce0a479550233b87ba391
sharan-vayakkady <134999723+sharan-vayakkady@users.noreply.github.com>
2023-06-09 11:05:20 +0530

    Update merge_conflict_check.sh

--- merge_conflict_check.sh
+++ merge_conflict_check.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-
 set -e
-
-git clone https://github.com/sharan-vayakkady/notify.git
-
-cd notify
-
-# Checkout the main branch
-git checkout main
-
-# Update the local repository
-git pull
-
-# Checkout the staging branch
-git checkout staging
-
-# Update the local repository
-git pull
-
-# Merge the main branch into staging
-if git merge-base --is-ancestor main HEAD; then
-  echo "No merge conflicts detected."
-test = 10
+git config user.name $USERNAME
+git config user.email $EMAIL
+git config pull.rebase false
+git fetch --all
+git branch
+git checkout $ORIGIN_BRANCH 
+git pull origin $ORIGIN_BRANCH
+git checkout -b $MERGE_BRANCH origin/$MERGE_BRANCH
+git pull origin $MERGE_BRANCH
+git branch
+git merge --no-commit --no-ff $ORIGIN_BRANCH  | tee automerge.out
+git branch
+if grep "Automatic merge failed" automerge.out; then
+    echo "Merge conflict detected"
+    # Generate a patch of the merge conflict
+    patch=$(git diff --name-only --diff-filter=U)
+    # Get the full patch contents
+    patch_content=$(git diff -U0 $patch)
+    echo "Merge conflict detected, Please review files : $patch"
+    result=`echo ":warning: Merge conflict detected, Please review files!"`
+    echo $result
+    echo "COMPARE_RESULT=$result" >> $GITHUB_OUTPUT
+    git merge --abort
+elif grep "Already up to date" automerge.out; then
+    result=`echo ":white_check_mark: Nothing to merge, already up to date!"`
+    #echo "COMPARE_RESULT=$result" >> $GITHUB_OUTPUT
 else
-  echo "Merge conflict detected!"
-  test = 5
+    echo "No merge conflict, please merge master to staging"
+    result=`echo ":negative_squared_cross_mark: Master has new changes, Master can be merged to Staging!"`
+    echo "COMPARE_RESULT=$result" >> $GITHUB_OUTPUT
 fi
